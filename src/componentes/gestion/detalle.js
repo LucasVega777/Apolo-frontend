fe0c878b7a5b66afc50c1d7ac60a03d985a95d2d
Eduardo Cardenas <42193826+educardenas97@users.noreply.github.com>
2022-12-19 17:10:57 -0300

    Implementa listado de usuarios asociados

--- src/componentes/gestion/detalle.js
+++ src/componentes/gestion/detalle.js
@@ -2,7 +2,8 @@
 import { useQuery } from "@apollo/client";
 import { GET_PROYECTO_DETALLE } from "../../graphql/querys";
 
-export default DetalleProyecto = ({ idProyecto }) => {
+export const DetalleProyecto = (props) => {
+    const { idProyecto } = props;
     // View all projects in list format
     const { loading, error, data } = useQuery(GET_PROYECTO_DETALLE, {
         variables: { idProyecto: idProyecto },
@@ -12,12 +13,56 @@ export default DetalleProyecto = ({ idProyecto }) => {
 
     // Create a list of project in select format
     // render the details of the project
+    // render the name of users in the project if exist
     return (
         <div>
-            <h1>Detalles del proyecto</h1>
-            <p>Descripcion: {data.proyectoByIdProyecto.descripcion}</p>
-            <p>Fecha de inicio: {data.proyectoByIdProyecto.fechaInicio}</p>
-            <p>Fecha de fin: {data.proyectoByIdProyecto.fechaFin}</p>
+            <p>
+                Fechas: {data.projectByIdProyecto.fechaInicio} -
+                {data.projectByIdProyecto.fechaFin}
+            </p>
+            <h3>Backlog</h3>
+            {data.projectByIdProyecto.backlogsByIdProyecto.nodes.map(
+                (backlog) => (
+                    <div key={backlog.idBacklog}>
+                        <p>Descripcion: {backlog.descripcion}</p>
+                        <p>Fecha inicio: {backlog.fechaInicio}</p>
+                        <p>Fecha fin: {backlog.fechaFin}</p>
+                        <p>
+                            <h4>Sprint</h4>
+                            {backlog.sprintsByIdBacklog.nodes.map((sprint) => (
+                                <div key={sprint.idSprint}>
+                                    <p>Descripcion: {sprint.descripcion}</p>
+                                    <p>Fecha inicio: {sprint.fechaInicio}</p>
+                                    <p>Fecha fin: {sprint.fechaFin}</p>
+                                    <p>Estado: {sprint.estado}</p>
+                                </div>
+                            ))}
+                        </p>
+                    </div>
+                )
+            )}
+
+            <p>
+                <h3>Usuarios</h3>
+                Cantidad:{" "}
+                {data.projectByIdProyecto.usersProjectsByIdProyecto.totalCount}
+                <ul>
+                    {data.projectByIdProyecto.usersProjectsByIdProyecto.nodes.map(
+                        (user) => (
+                            <li key={user.userByIdUser.idUser}>
+                                {user.userByIdUser.name}
+                                <button
+                                    onClick={() => {
+                                        console.log("Eliminar usuario");
+                                    }}
+                                >
+                                    Eliminar
+                                </button>
+                            </li>
+                        )
+                    )}
+                </ul>
+            </p>
         </div>
     );
-}
+};
